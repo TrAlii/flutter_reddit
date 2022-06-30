8ab4cc1f38b42c88644e7b4dc9bd584eac7b9e85
Arian <mha.september.2003@gmail.com>
2022-06-30 23:58:48 +0430

    server main completed

--- reddit/server/src/Main.java
+++ reddit/server/src/Main.java
@@ -1,6 +1,17 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Server server = null;
+        try (FileInputStream fis = new FileInputStream("DB\\Server.bin"); ObjectInputStream ois = new ObjectInputStream(fis)){
+            server = (Server) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(server != null) {
+            server.startServer();
+        }
     }
 }
