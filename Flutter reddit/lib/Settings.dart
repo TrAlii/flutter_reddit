968c6c06814583164f353123ab101091b3c34841
alizrnh <105929975+alizrnh@users.noreply.github.com>
2022-07-01 00:57:06 +0430

    Update Settings.dart

--- Flutter reddit/lib/Settings.dart
+++ Flutter reddit/lib/Settings.dart
@@ -11,7 +11,7 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Settings',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       home: SettingsPage(),
