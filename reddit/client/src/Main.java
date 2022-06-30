81cfbba6c5ef4ce9ffb25a24ae081a24b200394d
Arian <mha.september.2003@gmail.com>
2022-07-01 00:00:04 +0430

    client main completed

--- reddit/client/src/Main.java
+++ reddit/client/src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client("127.0.0.1", 5500);
+        client.start();
     }
 }
